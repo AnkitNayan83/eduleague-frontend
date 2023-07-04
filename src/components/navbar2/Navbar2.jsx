50bc28c3938496d325b0454626f003d8e6c7a7b4
Ankit Nayan <ankitnayan83@gmail.com>
2023-07-04 13:19:06 +0530

    commit

--- src/components/navbar2/Navbar2.jsx
+++ src/components/navbar2/Navbar2.jsx
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./navbar2.scss";
-import MenuIcon from "@mui/icons-material/Menu";
-import { ArrowBackIos, Close, NavigateNext } from "@mui/icons-material";
-import { useDispatch, useSelector } from "react-redux";
+import { ArrowBackIos, NavigateNext } from "@mui/icons-material";
+import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import { removeUser } from "../../redux/slice/authSlice";
 
 export const Navbar2 = ({ pageName }) => {
   const navigate = useNavigate();
-  const user = useSelector((state) => state.auth.user);
   const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
 
   const handelLogOut = () => {
     localStorage.clear();
