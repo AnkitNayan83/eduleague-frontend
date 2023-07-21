731dbc3c855f7109de718246898eda7ca224a958
Ankit Nayan <ankitnayan83@gmail.com>
2023-07-21 11:07:51 +0530

    commit

--- src/pages/joinQuiz/JoinQuiz.jsx
+++ src/pages/joinQuiz/JoinQuiz.jsx
@@ -12,32 +12,46 @@ export const JoinQuiz = () => {
   const [selectedQuizType, setSelectedQuizType] = useState("");
   const dispatch = useDispatch();
   const loading = useSelector((state) => state.alerts.loading);
+  const [topic, setTopic] = useState("");
+  const [course, setCourse] = useState("");
+  const [subject, setSubject] = useState("");
 
   useEffect(() => {
     const getQuiz = async () => {
       const token = localStorage.getItem("token");
       try {
         dispatch(showLoading());
-        const { data } = await axiosRequest.get("/quiz/get-quizes", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        const { data } = await axiosRequest.get(
+          `/quiz/get-quizes?course=${course}&subject=${subject}&topic=${topic}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
         dispatch(hideLoading());
         setSelectedQuizType("community");
+        console.log(data);
         setQuizes(data);
       } catch (error) {
+        setQuizes([]);
         dispatch(hideLoading());
         console.log(error);
       }
     };
     getQuiz();
-    // eslint-disable-next-line
-  }, []);
+  }, [course, subject, topic]);
 
   const handleQuizTypeClick = (quizType) => {
     setSelectedQuizType(quizType);
   };
+
+  const handelClear = () => {
+    setCourse("");
+    setTopic("");
+    setSubject("");
+  };
+
   return (
     <div className="background">
       <Navbar2 pageName={"Join Quiz"} />
@@ -61,27 +75,98 @@ export const JoinQuiz = () => {
           </div>
         </div>
         <div className="right-part">
-          {loading && (
+          {loading ? (
             <div className="spin">
               <h2>Loading please wait</h2> <PropagateLoader color="#fff" />
             </div>
-          )}
-          {selectedQuizType === "single" && (
+          ) : (
             <>
-              <h4 className="join-h4">Single Quiz</h4>
-              {quizes.singleQuizes &&
-                quizes.singleQuizes.map((quiz) => (
-                  <QuizCard key={quiz._id} quiz={quiz} />
-                ))}
-            </>
-          )}
-          {selectedQuizType === "community" && (
-            <>
-              <h4 className="join-h4">Community Quiz</h4>
-              {quizes.communityQuizes &&
-                quizes.communityQuizes.map((quiz) => (
-                  <QuizCard key={quiz._id} quiz={quiz} />
-                ))}
+              <div className="quiz__filter">
+                <select
+                  name="course"
+                  id="course"
+                  onChange={(e) => setCourse(e.target.value)}
+                >
+                  <option value="" selected={course === ""}>
+                    Select Course
+                  </option>
+                  <option
+                    value="12th (CBSE)"
+                    selected={course === "12th (CBSE)"}
+                  >
+                    12th
+                  </option>
+                  <option value="Cricket" selected={course === "Cricket"}>
+                    Cricket
+                  </option>
+                  <option
+                    value="General Knowledge"
+                    selected={course === "General Knowledge"}
+                  >
+                    General Knowledge
+                  </option>
+                </select>
+                <select
+                  name="subject"
+                  id="subject"
+                  onChange={(e) => setSubject(e.target.value)}
+                >
+                  <option value="" selected={subject === ""}>
+                    Select Subject
+                  </option>
+                  <option value="Physics" selected={subject === "Physics"}>
+                    Physics
+                  </option>
+                  <option value="NA" selected={subject === "NA"}>
+                    NA
+                  </option>
+                </select>
+                <select
+                  name="topic"
+                  id="topic"
+                  onChange={(e) => setTopic(e.target.value)}
+                >
+                  <option value="" selected={topic === ""}>
+                    Select Topic
+                  </option>
+                  <option
+                    value="Miscellaneous"
+                    selected={topic === "Miscellaneous"}
+                  >
+                    Miscellaneous
+                  </option>
+                  <option value="NA" selected={topic === "NA"}>
+                    NA
+                  </option>
+                </select>
+                <span className="cf" onClick={handelClear}>
+                  Clear Filter
+                </span>
+              </div>
+              {selectedQuizType === "single" && (
+                <>
+                  <h4 className="join-h4">Single Quiz</h4>
+                  {quizes?.singleQuizes.length === 0 && (
+                    <h2 className="not__found">Quizzes not found</h2>
+                  )}
+                  {quizes?.singleQuizes &&
+                    quizes?.singleQuizes.map((quiz) => (
+                      <QuizCard key={quiz._id} quiz={quiz} />
+                    ))}
+                </>
+              )}
+              {selectedQuizType === "community" && (
+                <>
+                  <h4 className="join-h4">Community Quiz</h4>
+                  {quizes?.communityQuizes.length === 0 && (
+                    <h2 className="not__found">Quizzes not found</h2>
+                  )}
+                  {quizes?.communityQuizes &&
+                    quizes?.communityQuizes.map((quiz) => (
+                      <QuizCard key={quiz._id} quiz={quiz} />
+                    ))}
+                </>
+              )}
             </>
           )}
         </div>
