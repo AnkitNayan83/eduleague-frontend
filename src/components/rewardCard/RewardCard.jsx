0c4e7cf9d9eb4e827347278e983c57bbf35a34d7
Ankit Nayan <ankitnayan83@gmail.com>
2023-08-01 10:42:31 +0530

    commit

--- src/components/rewardCard/RewardCard.jsx
+++ src/components/rewardCard/RewardCard.jsx
@@ -1,15 +1,74 @@
 import React from "react";
 import "./rewardCard.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { CheckCircleOutline } from "@mui/icons-material";
+import { toast } from "react-toastify";
+import { hideLoading, showLoading } from "../../redux/slice/alertSlice";
+import { axiosRequest } from "../../axiosInstance";
 
-export const RewardCard = ({ img, name, desc, price }) => {
+export const RewardCard = ({ img, name, desc, price, rewardId }) => {
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+
+  const isBought = () => {
+    const arr = user?.rewards;
+    for (let i = 0; i < arr?.length; i++) {
+      if (rewardId === arr[i]) return true;
+    }
+
+    return false;
+  };
+
+  const handelBought = async () => {
+    try {
+      if (user?.balance < price) {
+        toast.error("You dont have enough coins to purchase this");
+        return;
+      }
+      if (
+        window.confirm(
+          `Are you sure you want to purchase ${name} for ${price} coins`
+        )
+      ) {
+        dispatch(showLoading());
+        const { data } = await axiosRequest.put(
+          `/reward/${rewardId}`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        console.log(data);
+        dispatch(hideLoading());
+        window.location.reload(false);
+      } else {
+        dispatch(hideLoading());
+        console.log("nope");
+        return;
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      console.log(error);
+    }
+  };
+
   return (
     <div className="RewardCard">
       <img className="rew-img" src={img} alt="" />
       <h3>{name}</h3>
       <p>{desc}</p>
-      <button>
-        <img src="./images/dc.png" alt="" /> {price}
-      </button>
+      {!isBought() ? (
+        <button onClick={handelBought}>
+          <img src="./images/dc.png" alt="" /> {price}
+        </button>
+      ) : (
+        <div className="paid">
+          <CheckCircleOutline />
+          <span>Bought</span>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/rewards/Rewards.jsx
+++ src/pages/rewards/Rewards.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from "react";
 import { Navbar2 } from "../../components/navbar2/Navbar2";
 import "./rewards.scss";
 import { RewardCard } from "../../components/rewardCard/RewardCard";
-import { rewardsData } from "./data";
 import { toast } from "react-toastify";
 import { axiosRequest } from "../../axiosInstance";
+import { HashLoader } from "react-spinners";
+import { useSelector } from "react-redux";
 
 export const Rewards = () => {
   const [active, setActive] = useState(true);
   const [data, setData] = useState([]);
+  const loading = useSelector((state) => state.alerts.loading);
 
   useEffect(() => {
     const getRewards = async () => {
@@ -33,49 +35,60 @@ export const Rewards = () => {
       <Navbar2 pageName={"Rewards"} />
       <div className="wrapper">
         <div className="container">
-          <div className="left">
-            <div
-              className={"rew-option" + (active ? " active" : "")}
-              onClick={() => setActive(true)}
-            >
-              Reward Items
+          {loading ? (
+            <div className="rew__load">
+              <HashLoader />
+              <p>Loading please wait</p>
             </div>
-            <div
-              className={"rew-option" + (!active ? " active" : "")}
-              onClick={() => setActive(false)}
-            >
-              Coupon Code
-            </div>
-          </div>
-          <div className="right">
-            {active
-              ? data?.map((item, i) => {
-                  if (item.type === "reward")
-                    return (
-                      <RewardCard
-                        img={item.img}
-                        name={item.name}
-                        desc={item.desc}
-                        price={item.price}
-                        key={i}
-                      />
-                    );
-                  else return null;
-                })
-              : data?.map((item, i) => {
-                  if (item.type !== "reward")
-                    return (
-                      <RewardCard
-                        img={item.img}
-                        name={item.name}
-                        desc={item.desc}
-                        price={item.price}
-                        key={i}
-                      />
-                    );
-                  else return null;
-                })}
-          </div>
+          ) : (
+            <>
+              <div className="left">
+                <div
+                  className={"rew-option" + (active ? " active" : "")}
+                  onClick={() => setActive(true)}
+                >
+                  Reward Items
+                </div>
+                <div
+                  className={"rew-option" + (!active ? " active" : "")}
+                  onClick={() => setActive(false)}
+                >
+                  Coupon Code
+                </div>
+              </div>
+              <div className="right">
+                {active
+                  ? data?.map((item, i) => {
+                      if (item.type === "reward")
+                        return (
+                          <RewardCard
+                            img={item.img}
+                            name={item.name}
+                            desc={item.desc}
+                            price={item.price}
+                            rewardId={item._id}
+                            key={i}
+                          />
+                        );
+                      else return null;
+                    })
+                  : data?.map((item, i) => {
+                      if (item.type !== "reward")
+                        return (
+                          <RewardCard
+                            img={item.img}
+                            name={item.name}
+                            desc={item.desc}
+                            price={item.price}
+                            rewardId={item._id}
+                            key={i}
+                          />
+                        );
+                      else return null;
+                    })}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
